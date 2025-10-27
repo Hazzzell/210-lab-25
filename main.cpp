daf11e9b3fd466b67b816eab5d37cc511722640c
Hazzzell <hazzzelHZ@gmail.com>
2025-10-26 21:22:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <set>
 #include <chrono>
+#include <iomanip>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
-    
+    string fileName = "codes.txt";
 
+    vector<string> v;
+    list<string> l;
+    set<string> s;
+
+    cout << setw(10) << "Operation" 
+         << setw(10) << "Vector"
+         << setw(10) << "List" 
+         << setw(10) << "Set" << endl;
+
+    auto start = 
     return 0;
 }
 
