846175d1ae2cfb4d5b79ebe5f65b0fd798cc493f
Hazzzell <hazzzelHZ@gmail.com>
2025-10-26 21:28:22 -0700

    Update main.cpp
    Added code to measure reading times for vector, list, and set.

--- main.cpp
+++ main.cpp
@@ -20,13 +20,32 @@ int main() {
          << setw(10) << "List" 
          << setw(10) << "Set" << endl;
 
-    auto start = 
+    // first race: READING
+    auto start = high_resolution_clock::now();
+    ifstream f1(fileName); string code;
+    while(f1 >> code) v.push_back(code);
+    f1.close();
+    auto end = high_resolution_clock::now();
+    long long v_read = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    ifstream f2(fileName);
+    while(f2 >> code) l.push_back(code);
+    f2.close();
+    end = high_resolution_clock::now();
+    long long l_read = duration_cast<microseconds>(end - start).count();
+
+    start = high_resolution_clock::now();
+    ifstream f3(fileName);
+    while(f3 >> code) s.insert(code);
+    f3.close();
+    end = high_resolution_clock::now();
+    long long s_read = duration_cast<microseconds>(end - start).count();
+
+    cout << setw(10) << "Read" 
+         << setw(10) << v_read 
+         << setw(1) << l_read 
+         << setw(12) << s_read << endl;
+
     return 0;
 }
-
-/* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
